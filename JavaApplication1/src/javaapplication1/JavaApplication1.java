c61ece3928549607dde6513d0dc222f57ee30ff8
laboratorio <laboratorio@172.20.3.129>
2015-06-23 13:55:22 -0500

    java dia 03

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -10,7 +10,15 @@ public class JavaApplication1 {
     public static void main(String[] args) {
             
             
-      
+            String Valor=JOptionPane.showInputDialog("di tu nombre");
+            
+     
+
+    if (Valor != null){
+        
+    
+        JOptionPane.showMessageDialog(null, "hola  !"+Valor +"mucho gusto");
+    }
     }
     
     
